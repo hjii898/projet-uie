b5f40093125b734837ac25b2d93dad1946ec252e
wanehaoua21-eng <wanehaoua21@gmail.com>
2026-06-03 18:09:09 +0000

    Update hive.py

--- hive.py
+++ hive.py
@@ -1,1 +1,6 @@
-
+def hive():
+  import pandas as pd
+  df = pd.read_csv("hdfs_insurance.csv)
+  df = df.drop_duplicates()
+  df.to_csv("resultat.csv", index=False)
+  print ("Transformation Hive terminée)
