9687aabdc2822723f0dbc739a6e2cfd9c80898ac
hjii898 <hjii898@users.noreply.github.com>
2026-06-06 12:06:09 +0000

    Ajout fonction soustraction et tests

---
+++ dags/test_main.py
@@ -0,0 +1,8 @@
+def soustraction(a, b):
+    return a - b
+
+assert soustraction(10, 5) == 5
+assert soustraction(10.5, 2.5) == 8.0
+assert soustraction(-10, 5) == -15
+
+print("ces test fonctiennent effectivement")
